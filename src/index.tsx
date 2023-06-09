83ee91d80457113af92b4a1533c916d4859c1828
TanyaPiv <peachpeachttt@gmail.com>
2023-06-09 14:59:18 +0300

    fix public index.html

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,18 @@
 import React from 'react';
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 import './index.css';
 import App from './App';
+import reportWebVitals from "./reportWebVitals"
 
 
-const application = (
+const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
+root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
 
-ReactDOM.render(application, document.getElementById("root"));
+reportWebVitals();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
